0a90cc9726d3d700baba56011ff334a61e0be80f
karthik <koolkarthik38@gmail.com>
2020-04-06 17:02:08 +0530

    Add and Update site complete

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -6,12 +6,147 @@ const Employee = mongoose.model('Employee');
 const Admin = mongoose.model('admin');
 const Buildingsite = mongoose.model('building');
 //test
+
+
+
+
+
+
+router.post('/addSite/:name', (req,res) => {
+  res.set('Access-Control-Allow-Headers', '*');
+   Buildingsite.find({ 'buildingName': req.params.name },(err,docs) => {
+
+
+    if(!err){
+      if(docs.length == 0){
+        console.log("doc is null");
+        res.send("building not found");
+      }else{
+        // kosmo one exists
+        var siteExists = false;
+          siteArr = [];
+          for(var i = 0 ; i< docs[0].buildingSites.length ; i++){
+            console.log(docs[0].buildingSites[i].Site[0].siteName);
+            siteArr = siteArr.concat(docs[0].buildingSites[i].Site[0].siteName);
+            }
+
+            console.log(siteArr);
+
+            if(siteArr.includes(req.body.Site[0].siteName)){
+              siteExists = true;
+            }
+
+          if(siteExists){
+        //    res.send("this site already exists  , updating existing site");
+            console.log("this site already exists , updating existing site");
+            var newBuildingID = new Set();
+            var newOfficeName = new Set();
+            var index;
+            newBuildingID = req.body.Site[1].buildingId;
+            newOfficeName = req.body.Site[2].OfficeNames;
+
+            console.log("new building ids " + newBuildingID  );
+            console.log("new office name " + newOfficeName);
+
+            var earr;
+            for(var i = 0 ; i< docs[0].buildingSites.length ; i++){
+                if(docs[0].buildingSites[i].Site[0].siteName == req.body.Site[0].siteName){
+                  index = i;
+                  earr = docs[0].buildingSites[i];
+                  break;
+                }
+              }
+
+
+              console.log("earr  " + JSON.stringify(earr));
+              console.log("index is " + i);
+
+
+              console.log("after concating");
+            //  console.log(earr.Site[1].buildingId);
+              earr.Site[1].buildingId = earr.Site[1].buildingId.concat(newBuildingID);
+              earr.Site[2].OfficeNames = earr.Site[2].OfficeNames.concat(newOfficeName);
+              console.log("earr  " + JSON.stringify(earr));
+
+              //////// Replacement commeth
+                var arr = [];
+                arr = docs[0].buildingSites;
+                console.log(" \n object before pushing body \n");
+                console.log(docs[0].buildingSites);
+                console.log(" \n arr before pushing body \n");
+                console.log(arr);
+                arr[index] = earr;
+                console.log(" \n arr after pushing body \n");
+                console.log(arr);
+                docs[0].buildingSites = arr;
+                console.log("\n docs after pushing body \n");
+                console.log(docs[0]);
+                newDoc = docs[0];
+                console.log("\n doc ID is \n");
+                console.log(docs[0].id)
+                addSite( docs[0]._id , newDoc , res);
+
+
+
+
+          }
+          else {
+
+          var bidArr = [];
+          var flag = true;
+          for(var i = 0 ; i< docs[0].buildingSites.length ; i++){
+              console.log(docs[0].buildingSites[i].Site[1].buildingId);
+              bidArr = bidArr.concat(docs[0].buildingSites[i].Site[1].buildingId);
+            }
+
+            console.log("All buildingIDs" +  bidArr);
+
+            for(var i = 0 ; i < req.body.Site[1].buildingId.length ; i++){
+              if(bidArr.includes(req.body.Site[1].buildingId[i])){
+                flag = false;
+                break;
+              }
+            }
+
+            if(flag){
+              var arr = [];
+              arr = docs[0].buildingSites;
+              console.log(" \n object before pushing body \n");
+              console.log(docs[0].buildingSites);
+              console.log(" \n arr before pushing body \n");
+              console.log(arr);
+              arr.push(req.body);
+              console.log(" \n arr after pushing body \n");
+              console.log(arr);
+              docs[0].buildingSites = arr;
+              console.log("\n docs after pushing body \n");
+              console.log(docs[0]);
+              newDoc = docs[0];
+              console.log("\n doc ID is \n");
+              console.log(docs[0].id)
+              addSite( docs[0]._id , newDoc , res);
+            }{
+              res.send("Building ID already exists in a different building or site, Enter unique ID or contact KONE ");
+        }
+      }
+
+    }
+
+  }else{
+    res.send(err);
+    console.log(err);
+  }
+  });
+});
+
+
 router.get('/', (req,res) => {
   res.send('empty response')
 });
 
 router.post('/listEmployeeSite', (req,res) => {
     res.set('Access-Control-Allow-Headers', '*');
+
   Employee.find({ 'sites': req.body.site }, function (err, docs) {
     if(!err){
     console.log("site -> employee shown to user");
@@ -83,6 +218,28 @@ router.get('/listOfficeId', (req,res) => {
 });
 
 
+router.get('/listBuildingId', (req,res) => {
+  res.set('Access-Control-Allow-Headers', '*');
+   Buildingsite.find((err,docs) => {
+    if(!err){
+    console.log("complete doc shown to user");
+//    var obj = JSON.parse(docs);
+//    console.log(obj.buildingName);
+ var arr = [];
+ for(var i = 0 ; i< docs[0].buildingSites.length ; i++){
+     console.log(docs[0].buildingSites[i].Site[1].buildingId);
+     arr = arr.concat(docs[0].buildingSites[i].Site[1].buildingId);
+  }
+  res.json(arr);
+  }else{
+
+    res.send(err);
+    console.log(err);
+  }
+  });
+});
+
+
 router.get('/listOffice', (req,res) => {
   res.set('Access-Control-Allow-Headers', '*');
    Buildingsite.find((err,docs) => {
@@ -93,7 +250,9 @@ router.get('/listOffice', (req,res) => {
  var arr = [];
  for(var i = 0 ; i< docs[0].buildingSites.length ; i++){
      console.log(docs[0].buildingSites[i].Site[2].OfficeNames);
+  //   if(!arr.includes(docs[0].buildingSites[i].Site[2].OfficeNames)){
      arr = arr.concat(docs[0].buildingSites[i].Site[2].OfficeNames);
+     arr = [...new Set(arr)];
   }
   res.json(arr);
   }else{
@@ -291,6 +450,26 @@ Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, 
     res.send("invalid email");
     console.log("invalid email");
   }
+}
+
+function addSite( id , newDoc , res) {
+Buildingsite.findOneAndUpdate({ _id : id }, newDoc , { new: true }, (err, doc) => {
+      if (!err) {
+        //res.send("record updated with  \n" + JSON.stringify(newDoc));
+        res.json(newDoc);
+    }
+        else {
+            if (err.name == 'ValidationError') {
+              console.log("volidation error");
+              res.send("validation error "+ err);
+
+            }
+            else{
+                console.log('Error during record update : ' + err);
+                  res.send('Error during record update : ' + err);
+        }}
+    });
+
 }
 
 
